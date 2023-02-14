88ace9dc2588717a09403adf6e97e0285b620e30
LucasHabakuc1 <92757128+LucasHabakuc1@users.noreply.github.com>
2023-02-14 10:04:43 -0300

    Importando os novos arquivos

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+})
     .then(() => {
-        console.log('Conectei a base de dados.')
         app.emit('pronto');
     })
     .catch(e => console.log(e));
@@ -13,24 +17,24 @@ const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middleware/middlerware');
 
-app.use(helmet());
+// app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-    secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+    secret: 'akasdf()',
     store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-      httpOnly: true
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
     }
-  });
+});
 
 app.use(sessionOptions);
 app.use(flash());
--- src/middleware/middlerware.js
+++ src/middleware/middlerware.js
@@ -1,5 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local.'
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
     next();
 };
 
@@ -17,4 +19,14 @@ exports.checkCsrfError = (err, req, res, next) => {
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next()
-}
+};
+
+exports.loginRequired = ( req, res, next) => {
+    if(!req.session.user) {
+        req.flash('errors', 'Você precisar acessar.');
+        req.session.save(() => res.redirect('/'));
+        return;
+    }
+
+    next();
+};
